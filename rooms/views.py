c221bab189befffc80f911cde04eb4b43b665ec0
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-22 04:50:40 +0900

    :memo: #11.5 | get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,16 @@
-from math import ceil
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))
     print(vars(rooms.paginator))
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": rooms},
+        context={"page": rooms},
     )
