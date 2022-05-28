5a090b9cf0b10aed62cdb63428afddfe075baf2a
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-28 21:54:21 +0900

    :memo: #12.2 | room_detail FBV finished
    - handle not found room

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import redirect, render
 from . import models
 
 
@@ -15,5 +16,8 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    room = models.Room.objects.get(pk=pk)
-    return render(request, "rooms/detail.html", {"room": room})
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
