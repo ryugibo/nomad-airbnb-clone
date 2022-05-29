79c81133c7ffdae71097b789bdb08d31a4f1cc27
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-29 12:07:01 +0900

    :memo: #13.7 | Introduction to Django Forms

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django_countries import countries
 from django.shortcuts import render
-from . import models
+from . import models, forms
 
 
 class HomeView(ListView):
@@ -23,72 +22,6 @@ class RoomDetail(DetailView):
 
 
 def Search(request):
-    city = request.GET.get("city", "Anywhere")
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    price = int(request.GET.get("price", 0))
-    guests = int(request.GET.get("guests", 0))
-    beds = int(request.GET.get("beds", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.getlist("amenities")
-    s_facilities = request.GET.getlist("facilities")
-    instant = bool(request.GET.get("instant", False))
-    superhost = bool(request.GET.get("superhost", False))
 
-    form = {
-        "city": city,
-        "s_country": country,
-        "s_room_type": room_type,
-        "price": price,
-        "guests": guests,
-        "beds": beds,
-        "bedrooms": bedrooms,
-        "baths": baths,
-        "s_amenities": s_amenities,
-        "s_facilities": s_facilities,
-        "instant": instant,
-        "superhost": superhost,
-    }
-
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
-
-    choices = {
-        "countries": countries,
-        "room_types": room_types,
-        "amenities": amenities,
-        "facilities": facilities,
-    }
-
-    filter_args = {}
-
-    if city != "Anywhere":
-        filter_args["city__startswith"] = city
-
-    filter_args["country"] = country
-
-    if room_type != 0:
-        filter_args["room_type__pk"] = room_type
-
-    if instant is True:
-        filter_args["instant_book"] = True
-
-    if superhost is True:
-        filter_args["host__superhost"] = True
-
-    for s_amenity in s_amenities:
-        filter_args["amenities__pk"] = int(s_amenity)
-
-    for s_facility in s_facilities:
-        filter_args["facilities__pk"] = int(s_facility)
-
-    rooms = models.Room.objects.filter(**filter_args)
-
-    return render(
-        request,
-        "rooms/search.html",
-        {**form, **choices, "rooms": rooms},
-    )
+    form = forms.SearchForm
+    return render(request, "rooms/search.html", {"form": form})
